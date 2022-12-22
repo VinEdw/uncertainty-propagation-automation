17a922f1312002972e4b1a0ce558aa71333f6ea4
Vincent Edwards <mister.man489@gmail.com>
2022-12-22 15:41:48 -0800

    messy integration of print_results function

--- main.py
+++ main.py
@@ -2,130 +2,11 @@ import sys
 import math
 from instruction_loader import load_instructions
 from instruction_processor import process_instructions
-
-def get_leading_place_value(value):
-    """Get the leading place value of the input number."""
-    # The one's place would be 0; The hundredth's place would be -2; The thousand's place would be 3
-    # Ten raised to what power gives the order of magnitude of the number
-    if value == 0:
-        return 0 # If the number is 0, the formula breaks, so just return zero
-    else:
-        return math.floor(math.log(abs(value), 10))
-
-def round_uncertainty(uncertainty):
-    """Round the uncertainty to one significant figure."""
-    place_value = get_leading_place_value(uncertainty)
-    return round(uncertainty, -place_value)
-
-def round_value_to_uncertainty(value, uncertainty):
-    """Round a value to match the given uncertainty."""
-    # If the uncertainty is 0, return the unrounded value
-    if uncertainty == 0:
-        return value
-    # If the uncertainty is larger than the value, return the value to 2 significant figures
-    if uncertainty > abs(value):
-        value_pv = get_leading_place_value(value)
-        return round(value, -(value_pv-1))
-    # Otherwise, round the value to the same place value as the uncertainty
-    rounded_uncertainty = round_uncertainty(uncertainty)
-    uncertainty_pv = get_leading_place_value(rounded_uncertainty)
-    return round(value, -uncertainty_pv)
-
-def get_symbol_from_data(data, symbol):
-    """Grab the dictionary for the given symbol string from the json data."""
-    for item in data["inputs"] + data["outputs"]:
-        if symbol == item["symbol"]:
-            return item
-    return None
-
-def pretty_print_data(data, columns=None):
-    """Make a string form of the processed data in a nice table."""
-    table = []
-    pm = "±"
-    if columns == None:
-        item_list = data["inputs"] + data["outputs"]
-    else:
-        item_list = [get_symbol_from_data(data, symbol) for symbol in columns]
-
-    for item in item_list:
-        symbol = item["symbol"]
-        units = item["units"]
-        header = f"{symbol} ({units})"
-        column_strs = [header]
-        # Round each value/uncertainty pair and put them in a string together
-        # Collect these strings in a list
-        for value, uncertainty in zip(item["value"], item["uncertainty"]):
-            rounded_value = round_value_to_uncertainty(float(value), float(uncertainty))
-            rounded_uncertainty = round_uncertainty(float(uncertainty))
-            column_strs.append(f"{rounded_value} {pm} {rounded_uncertainty}")
-        # Justify each of these strings to have the same length
-        max_length = max(len(string) for string in column_strs)
-        column_strs = [string.ljust(max_length) for string in column_strs]
-        # Add a horizontal bar after the header
-        column_strs.insert(1, "-" * max_length)
-        # Add the column string list to the table list
-        table.append(column_strs)
-    # Insert a trial number column at the start
-    row_count = len(table[0])
-    max_length = max(5, len(str(row_count)))
-    trial_column = ["Trial", "-" * max_length] + [str(i) for i in range(1, row_count + 1)]
-    trial_column = [string.ljust(max_length) for string in trial_column]
-    table.insert(0, trial_column)
-    # Construct the final string
-    final_str = ""
-    column_count = len(table)
-    for i in range(row_count):
-        row_data = []
-        for j in range(column_count):
-            cell = table[j][i]
-            row_data.append(cell)
-        row_str = " | ".join(row_data)
-        final_str += row_str + "\n"
-    return final_str[:-1]
-
-def print_latex_table(data, columns):
-    """Make a Latex version of the table string."""
-    # Assume these macros exist, among the other ones provided by the iopart class
-    # \\def\\mcm{r@{.}l@{ ± }r@{.}l}
-    # \\def\\mch#1{\multicolumn{4}{l}{#1}} 
-    starting_str = pretty_print_data(data, columns)
-    starting_str = starting_str.replace("|", "&") # Replace vertical bars with ampersands
-    starting_str = starting_str.replace(".", "&") # Replace decimal points with ampersands
-    starting_str = starting_str.replace("±", "&") # Replace plus or minus with ampersands
-    split_str = starting_str.split("\n")
-    split_str[1] = "\mr" # Replace the horizontal bar with "\mr"
-    # Add \\ to the end of each data row
-    for i in range(2, len(split_str)):
-        split_str[i] += "\\\\"
-    # Make the headers multicolumn
-    split_header = split_str[0].split(" & ")
-    column_count = len(split_header)
-    new_split_header = []
-    for header in split_header:
-        # Skip the trial header
-        if header.strip() == "Trial":
-            new_split_header.append("Trial ")
-            continue
-        header = header.strip()
-        middle_space_index = header.find(" ")
-        symbol = header[:middle_space_index]
-        units = header[middle_space_index:]
-        new_header = "\\mch{$" + symbol + "$" + units + "}"
-        new_split_header.append(new_header)
-    split_str[0] = "&".join(new_split_header) + "\\\\"
-    # Make the opening and closing tabular syntax
-    opening = "\\begin{table}[htbp]\n\caption{\label{}\n}\n"
-    opening += "\\begin{indented}\\lineup\\item[]\\begin{tabular}{@{}l" + "\\mcm" * (column_count - 1) + "}\n\\br\n"
-    closing = "\n\\br\n\\end{tabular}\\end{indented}\\end{table}"
-
-    final_str = opening + "\n".join(split_str) + closing
-    final_str = final_str.replace(" -", "\\-") # Fix minus signs without breaking column alignment
-    return final_str
-
+from result_printer import print_results
 
 if __name__ == "__main__":
-    # Open the input file name and load the instructions
     file_name = sys.argv[1]
+    # Default the table_format to "markdown"
     if len(sys.argv) == 3:
         table_format = sys.argv[2]
     else:
@@ -133,15 +14,4 @@ if __name__ == "__main__":
     
     instructions = load_instructions(file_name)
     process_instructions(instructions)
-
-    desired_tables = instructions.get("tables", None)
-    if desired_tables == None or len(desired_tables) == 0:
-        desired_tables = [None]
-    for column_list in desired_tables:
-        if table_format == "markdown":
-            pretty_str = pretty_print_data(instructions, column_list)
-            print(pretty_str)
-        elif table_format == "latex":
-            latex_str = print_latex_table(instructions, column_list)
-            print(latex_str)
-        print()
+    print_results(instructions, table_format)
--- result_printer.py
+++ result_printer.py
@@ -36,7 +36,7 @@ def get_measurement_str(value, uncertainty, separator="±"):
     else:
         uncertainty_pv = get_leading_place_value(rounded_uncertainty)
         rounded_value = round(value, -uncertainty_pv)
-    return f"{rounded_value} {separator} {uncertainty}"
+    return f"{rounded_value} {separator} {rounded_uncertainty}"
 
 def ljust_column(column):
     """
@@ -62,13 +62,26 @@ def item_to_column(item, header_format="{symbol} ({units})", separator="±", bar
     header = header_format.format(symbol=symbol, units=units)
     column.append(header)
     for value, uncertainty in zip(item["value"], item["uncertainty"]):
-        measurement_str = get_measurement_str(value, uncertainty, separator)
+        measurement_str = get_measurement_str(float(value), float(uncertainty), separator)
         column.append(measurement_str)
     column = ljust_column(column)
     if bar_after_header:
         max_length = len(column[0])
         bar = "-" * max_length
         column.insert(1, bar)
+    return column
+
+def get_trial_column(length, bar_after_header=True):
+    """
+    Get a list of left justified strings for the trial column.
+    This column has 'Trial' as the header and simply counts off the rows.
+    """
+    column = ["Trial"]
+    for i in range(length):
+        column.append(str(i + 1))
+    column = ljust_column(column)
+    if bar_after_header:
+        column.insert(1, "-" * len(column[0]))
     return column
 
 def get_item_list(instructions, symbols=None):
@@ -87,10 +100,56 @@ def get_item_list(instructions, symbols=None):
             item = get_item(instructions, symbol)
             item_list.append(item)
     return item_list
-        
+
+def get_table_str(item_list, table_format):
+    """
+    Turn the given item list into a table with the specified table format (markdown|latex)
+    """
+    if table_format == "markdown":
+        header_format = "{symbol} ({units})"
+        col_separator = " | "
+        pm_separator = "±"
+        bar_after_header = True
+    elif table_format == "latex":
+        header_format = "\\mch{{${symbol}$ ({units})}}"
+        col_separator = " & "
+        pm_separator = "&"
+        bar_after_header = False
+    column_list = [item_to_column(item, header_format, pm_separator, bar_after_header) for item in item_list]
+    trial_count = len(item_list[0]["value"])
+    column_list.insert(0, get_trial_column(trial_count, bar_after_header))
+    row_list = [[column[i] for column in column_list] for i in range(len(column_list[0]))]
+    row_strs = [col_separator.join(row) for row in row_list]
+    if table_format == "markdown":
+        table_str = "\n".join(row_strs)
+        return table_str
+    if table_format == "latex":
+        row_strs = [row + r" \\" for row in row_strs]
+        row_strs = [row.replace(" -", "\\-") for row in row_strs]
+        row_strs = [row.replace(".", "&") for row in row_strs]
+        row_strs.insert(1, "\\mr")
+        opening = "\\begin{table}[htbp]\n\caption{\label{}\n}\n"
+        opening += "\\begin{indented}\\lineup\\item[]\\begin{tabular}{@{}l"
+        item_count = len(item_list)
+        opening += "\\mcm" * item_count 
+        opening += "}\n\\br\n"
+        closing = "\n\\br\n\\end{tabular}\\end{indented}\\end{table}"
+        table_str = "\n".join(row_strs)
+        return opening + table_str + closing
 
 def print_results(instructions, table_format):
     """
     Based on a processed instructions object, print out the specified tables
-    Use the desired table format (markdown, latex).
+    Use the desired table format (markdown|latex).
+    
+    For the LaTeX format assume these macros exist, among the other ones provided by the iopart class
+    - \\def\\mcm{r@{.}l@{ ± }r@{.}l}
+    - \\def\\mch#1{\multicolumn{4}{l}{#1}} 
     """
+    desired_table_list = instructions["tables"]
+    if len(desired_table_list) == 0:
+        desired_table_list = [None]
+    for desired_table in desired_table_list:
+        item_list = get_item_list(instructions, desired_table)
+        print(get_table_str(item_list, table_format))
+        print()
