17b342c0afad22b4e1082781305e7ab371f414ea
Vincent Edwards <mister.man489@gmail.com>
2022-12-21 16:30:02 -0800

    instruction_processor initialized

---
+++ instruction_processor.py
@@ -0,0 +1,10 @@
+from sympy.parsing.sympy_parser import parse_expr
+
+def process_instructions(instructions):
+    """
+    Process the calculation instructions.
+    "instructions" is the processed json input file.
+    Note that the instructions object will be mutated in the process.
+    
+    Tasks include calculating the uncertainty for each input item, and calculating the value & uncertainty for each output item.
+    """
