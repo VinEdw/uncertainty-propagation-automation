25b7c9283b372393b3834eec8f661f954c0175da
Vincent Edwards <mister.man489@gmail.com>
2022-12-22 12:59:09 -0800

    result_printer.py initialized

---
+++ result_printer.py
@@ -0,0 +1,7 @@
+import math
+
+def print_results(instructions, table_format):
+    """
+    Based on a processed instructions object, print out the specified tables
+    Use the desired table format (markdown, latex).
+    """
